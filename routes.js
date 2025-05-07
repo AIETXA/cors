65bd11c14c0878a575fc8156791e00bd8dfd423d
Ailén <ailenechaguibel@gmail.com>
2025-05-07 08:46:29 +0200

    trabajo no termina, no puedo traerme la info de los personajes

--- routes.js
+++ routes.js
@@ -4,47 +4,40 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
+
+
 router.get('/character', async (req, res) => {
     const name = req.query.name;
-    let url = 'https://rickandmortyapi.com/api/character?name=rick';
+    let url = 'https://rickandmortyapi.com/api/character'
     
     if (name) {
-        url += `/?name=${encodeURIComponent(name)}`;
+        url += `?name=${encodeURIComponent(name)}`;
       }
     
-    try {
-        const allCharacters = []; 
-        while(url) {
-            const response = await axios.get(url)
-            const data = response.data;
-            allCharacters.push(...data.results);
-            url = data.info.next;
-        }
-       
-    res.json(allCharacters);
-    
-      } catch (ERROR) {
-        res.status(404).json({error: 'personaje no encontrado'});
-    
-      }
-    });
+      try {
+        const response = await axios.get(url);
+        const characters = response.data.results;
+        res.json(characters);
+    } catch (error) {
+        res.status(404).json({ error: 'Personaje no encontrado' });
+    }
+});
 
-
-    router.put('/character/:nombre', (req, res) => {
+   /* router.put('/character/:name', (req, res) => {
         const name = req.params.name;
-        const character = allCharacters.findIndex(c => c.name === name);
+        const character = characters.findIndex(c => c.name === name);
     
     if(character !== -1) {
-        allCharacters[character].status = req.body.status
-        allCharacters[character].species = req.body.species
-        allCharacters[character].gender = req.body.gender
-        allCharacters[character].image = req.body.image
-         res.json(allCharacters[character]);
+      characters[character].status = req.body.status
+      characters[character].species = req.body.species
+      characters[character].gender = req.body.gender
+      characters[character].image = req.body.image
+         res.json(characters[character]);
        
     } else {
         res.status(404).send('Personaje no encontrado');
     }
-    });
+    });*/
     
 module.exports = router;
 
